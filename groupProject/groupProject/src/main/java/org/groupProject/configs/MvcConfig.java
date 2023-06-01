fc162dfb22d95597c57f89e7b22a4fd4bc3770d6
Jongpyo Hong <djurev@naver.com>
2023-06-01 22:00:27 +0900

    security, static 경로 설정

---
+++ groupProject/groupProject/src/main/java/org/groupProject/configs/MvcConfig.java
@@ -0,0 +1,23 @@
+package org.groupProject.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Value("${file.upload.path}")
+    private String fileUploadPath;
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) { // 레이아웃 확인용
+        registry.addViewController("/").setViewName("main/index");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:///" + fileUploadPath);
+    }
+}
