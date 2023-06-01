b024033e3cd5e8c5cb4135b8a1eed35b6d73f406
Jongpyo Hong <djurev@naver.com>
2023-06-01 20:50:03 +0900

    06/01 group_project

---
+++ groupProject/groupProject/src/test/java/org/groupProject/GroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.groupProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
