8bdf53f0641d8c1b6c43100ce5646d4b97f8c2de
silverg <133085347+EunJi9739@users.noreply.github.com>
2023-06-02 11:47:50 +0900

    Add files via upload

---
+++ project0602/src/test/java/project0602/Project0602ApplicationTests.java
@@ -0,0 +1,13 @@
+package project0602;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project0602ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
