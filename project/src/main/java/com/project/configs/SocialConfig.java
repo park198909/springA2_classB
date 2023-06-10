0c1f5a497a7ae8ade12162de0780422aa30b5bd1
baekmir0505 <dudrms1123@naver.com>
2023-06-10 13:02:19 +0900

    06/10 작업내용

--- project/src/main/java/com/project/configs/SocialConfig.java
+++
@@ -1,7 +0,0 @@
-package com.project.configs;
-
-public class SocialConfig {
-    public static String restApiKey="1c257c7802994947e970b10a7c06cc76";
-
-    public static String restApiCallback="http://localhost:3000/social/login";
-}
