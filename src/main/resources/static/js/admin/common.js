43eca49012f5548905b4ed858f5e55da0389ff07
jongpyo-hong <djurev@naver.com>
2023-06-12 13:11:36 +0900

    06/12 작업내용

--- src/main/resources/static/js/admin/common.js
+++ src/main/resources/static/js/admin/common.js
@@ -15,3 +15,43 @@ window.addEventListener("DOMContentLoaded", function() {
     }
     /** 전체 선택 토글 처리 E */
 });
+
+/** 푸터 이용약관 */
+window.addEventListener("DOMContentLoaded", function() {
+    const terms_use = document.querySelectorAll('.footer_p p')
+    console.log(terms_use);
+    for (const el of terms_use) {
+        el.addEventListener('click', ()=> {
+            Swal.fire({
+                title : '이용안내',
+                html: '개인정보수집 범위 : 이름, 연락처.<br/> 개인정보수집 및 이용 목적: 온라인 문의 및 상담 자료와 결과 회신<br/>개인정보수집 및 보유기간 : 개인정보 수집 및 이용에 대한 목적이 달성되면 지체없이 파기하며 최대 보유기간은 1년을 넘기지 아니한다.'
+            })
+        });
+    }
+});
+
+window.addEventListener("DOMContentLoaded", function() {
+    const terms_use2 = document.querySelectorAll('.ad_explain1')
+    for (const el of terms_use2) {
+        el.addEventListener('click', ()=> {
+            Swal.fire({
+                icon : 'info',
+                title : '사이트 설정 안내',
+                html: ' 이용방법..... '
+            })
+        });
+    }
+});
+
+window.addEventListener("DOMContentLoaded", function() {
+    const terms_use3 = document.querySelectorAll('.ad_explain2')
+    for (const el of terms_use3) {
+        el.addEventListener('click', ()=> {
+            Swal.fire({
+                icon : 'info',
+                title : '게시판 관리 안내',
+                html: '이용방법..... '
+            })
+        });
+    }
+});
