55fe3d8fd7718227e7d04a26549e21155f72dea5
Jongpyo Hong <112396745+jongpyo-hong@users.noreply.github.com>
2023-06-12 21:10:05 +0900

    Delete springboard directory

--- springboard/src/main/java/org/koreait/repositories/FileInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package org.koreait.repositories;
-
-import org.koreait.entities.FileInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface FileInfoRepository extends JpaRepository<FileInfo, Long>, QuerydslPredicateExecutor<FileInfo> {
-}
