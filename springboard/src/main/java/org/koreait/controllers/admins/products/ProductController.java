4078647c2b90dbb828939b88a3b8f00898149cb6
yonggyo1125 <yonggyo00@kakao.com>
2023-06-12 16:40:56 +0900

    .

--- springboard/src/main/java/org/koreait/controllers/admins/products/ProductController.java
+++ springboard/src/main/java/org/koreait/controllers/admins/products/ProductController.java
@@ -1,21 +1,43 @@
 package org.koreait.controllers.admins.products;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.koreait.commons.MenuDetail;
+import org.koreait.commons.Menus;
 import org.koreait.models.product.ProductAddService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/admin/product")
 @RequiredArgsConstructor
 public class ProductController {
 
+    private final HttpServletRequest request;
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        commonProcess(model, "상품목록");
 
         return "admin/product/index";
+    }
+
+    private void commonProcess(Model model, String title) {
+        String URI = request.getRequestURI();
+
+        // 서브 메뉴 처리
+        String subMenuCode = Menus.getSubMenuCode(request);
+        model.addAttribute("subMenuCode", subMenuCode);
+
+        List<MenuDetail> submenus = Menus.gets("product");
+        model.addAttribute("submenus", submenus);
+
+        model.addAttribute("pageTitle", title);
+        model.addAttribute("title", title);
     }
 }
