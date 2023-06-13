ec6f9066ccd15d58d43a2f72696b5c8cecb6a05e
yonggyo1125 <yonggyo00@kakao.com>
2023-06-13 17:19:47 +0900

    소스 업로드

--- springboard/src/main/resources/static/js/admin/fileManager.js
+++ springboard/src/main/resources/static/js/admin/fileManager.js
@@ -27,17 +27,28 @@ window.addEventListener("DOMContentLoaded", function() {
     if (fileEl == null) { //없는 경우는 파일 DOM 추가
         fileEl = document.createElement("input");
         fileEl.type="file";
+        fileEl.multiple = true;
         fileManager.fileEl = fileEl;
+
     } else { // 있는 경우는 파일 값 초기화
         fileEl.value = "";
     }
 
     for (const el of uploadFiles) {
         el.addEventListener("click", function() {
-
-
+            fileEl.gid = el.dataset.gid;
+            fileEl.location = el.dataset.location;
             fileEl.click();
         });
     }
     /** 파일 업로드 버튼 처리 E */
+    /** 파일 선택 처리 S */
+    fileManager.fileEl.addEventListener("change", function(e) {
+        const files = this.files;
+        const gid = this.gid;
+        const location = this.location;
+
+        fileManager.upload(files, gid, location);
+    });
+    /** 파일 선택 처리 E */
 });
